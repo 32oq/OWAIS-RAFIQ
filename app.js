af21af3da2503b633e4d80132f39f8ab5134c790
owais rafiq <60499181+32oq@users.noreply.github.com>
2022-02-27 12:49:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-const navbrand=document.querySelector('nav');
-document.addEventListener('scroll', () => {
-navbrand.classList.toggle('brand1');
+var d2 = document.querySelector(".d2");
+var p = document.querySelector(".pdiv");
+d2.onclick = function() {
+    d2.classList.toggle("actived2");
+    darkmode();
+    if (p.innerHTML == "ON") {
+        p.innerHTML = "OFF";
+    } else {
+        p.innerHTML = "ON";
+    }
+}
 
-});
+function darkmode() {
+
+
+
+    var heading = document.querySelector(".navbar-brand");
+    var navbar = document.querySelector(".navbar");
+    var doc = document.body;
+    doc.classList.toggle("darkmode");
+    heading.classList.toggle("myname");
+    navbar.classList.toggle("navbar-dark");
+
+
+    p.classList.toggle("pdivactive");
+}
